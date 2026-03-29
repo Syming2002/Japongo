7caba9942074d3e277a5a59cbf81e8f3a6c6c4ef
Syming2002 <simon.bruneau@yahoo.com>
2026-03-29 18:40:21 +0200

    Change some stuff to make the pages visible

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import KanaPage from "./pages/KanaPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={"/Japongo/"}>
       <KanaProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
